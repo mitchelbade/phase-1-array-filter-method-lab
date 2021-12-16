b2861f405a5c677b98abef5c79b894266a82b3a1
mitchelbade <mitchelm.bade@gmail.com>
2021-12-15 16:38:43 -0800

    commit tree cf9e4e582c15b5820f1976265c69306b7bcced24

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    const matchingListOfDrivers = drivers.filter(element => element.toLowerCase() == string.toLowerCase())
+    return matchingListOfDrivers;
+}
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(element => element.startsWith(letter))
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(element => element.name === string || element.hometown === string)
+}
